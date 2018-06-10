5bea4bc6f84e847f9acf56c4f994c0719277f0ef
Huseyin Altas <altas2001@hotmail.com>
2018-06-10 00:45:07 -0500

    all is added

---
+++ src/ourproject/Reverse.java
@@ -0,0 +1,14 @@
+package ourproject;
+
+public class Reverse {
+	
+	public static void main(String[] args) {
+		
+		String str="Hello world!";
+		
+		StringBuilder rvrs=new StringBuilder(str);
+		
+		System.out.println(rvrs.reverse());
+	}
+
+}
